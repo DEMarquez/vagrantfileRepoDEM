7a8680a9ec4d0b104f6b8acf264b50c54c3d2186
Daniel <daniel.espimarquez06@gmail.com>
2017-01-18 11:12:14 +0100

    suvimos C3

--- provision.sh
+++ provision.sh
@@ -1,1 +1,13 @@
 apt-get -y install apache2
+cd ..
+cd ..
+sudo cp -r /vagrant/sitio1 /var/www/
+sudo cp -r /vagrant/sitio2 /var/www/
+sudo a2dissite default
+sudo a2enmod vhost_alias
+sudo service apache2 restart
+sudo cp -r /vagrant/config/sitio1 /etc/apache2/sites-available/
+sudo cp -r /vagrant/config/sitio2 /etc/apache2/sites-available/
+sudo a2ensite sitio1
+sudo a2ensite sitio2
+sudo service apache2 restart
